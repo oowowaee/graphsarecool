b4a820fe134e815b4e09be1b83119d744e3a60cd
Tally Osborne <tallyoh@gmail.com>
2022-03-23 19:58:02 -0500

    Add basic graph

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-console.log("working");
+import * as d3 from 'd3';
+
+// Setup project:
+// https://code.likeagirl.io/how-to-set-up-d3-js-with-webpack-and-babel-7bd3f5e20df7
+// Add basic d3 graph:
+// https://d3-graph-gallery.com/graph/barplot_basic.html
+
+// set the dimensions and margins of the graph
+// set the dimensions and margins of the graph
+const margin = {top: 30, right: 30, bottom: 70, left: 60},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+const svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+// Parse the Data
+d3.csv("./data.csv").then( function(data) {
+  console.log(data)
+
+  const maxValue = Math.max(...data.map(d => d.Value))
+  const roundedMaxValue = Math.round(maxValue / 100) * 100 + 100
+
+  // X axis
+  const x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(data.map(d => d.Country))
+    .padding(0.2);
+  svg.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x))
+    .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+  // Add Y axis
+  const y = d3.scaleLinear()
+    .domain([0, roundedMaxValue])
+    .range([ height, 0]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // Bars
+  svg.selectAll("mybar")
+    .data(data)
+    .join("rect")
+      .attr("x", d => x(d.Country))
+      .attr("y", d => y(d.Value))
+      .attr("width", x.bandwidth())
+      .attr("height", d => height - y(d.Value))
+      .attr("fill", "#69b3a2")
+
+})
