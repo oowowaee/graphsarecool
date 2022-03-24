e839cf39de6f80534da345b5cf68713a6f212586
Tally Osborne <tallyoh@gmail.com>
2022-03-23 21:02:37 -0500

    Add title

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import * as d3 from 'd3';
 // Add basic d3 graph:
 // https://d3-graph-gallery.com/graph/barplot_basic.html
 
+// Add hover effects:
+// https://medium.com/@kj_schmidt/show-data-on-mouse-over-with-d3-js-3bf598ff8fc2
+
 // set the dimensions and margins of the graph
 const margin = {top: 30, right: 30, bottom: 150, left: 60},
     width = 460 - margin.left - margin.right,
@@ -20,10 +23,16 @@ const svg = d3.select("#my_dataviz")
 
 // Parse the Data
 d3.csv("./data.csv").then( function(data) {
-  console.log(data)
-
   const maxValue = Math.max(...data.map(d => d.Value))
   const roundedMaxValue = Math.round(maxValue / 100) * 100 + 100
+
+  svg.append("text")
+    .attr("x", (width / 2))
+    .attr("y", 0 - (margin.top / 2))
+    .attr("text-anchor", "middle")
+    .style("font-size", "18px")
+    .style("text-decoration", "underline")
+    .text("Awesome chart");
 
   // X axis
   const x = d3.scaleBand()
@@ -35,14 +44,8 @@ d3.csv("./data.csv").then( function(data) {
     .call(d3.axisBottom(x))
     .selectAll("text")
       .attr("transform", "translate(-10,0)rotate(-45)")
-      .style("text-anchor", "end");
-
-
-  // This chooses a color based on the value
-  // var colors = d3.scaleQuantize()
-  //   .domain([0, 900])
-  //   .range(["#5E4FA2", "#3288BD", "#66C2A5", "#ABDDA4", "#E6F598",
-  //   "#FFFFBF", "#FEE08B", "#FDAE61", "#F46D43", "#D53E4F", "#9E0142"]);
+      .style("text-anchor", "end")
+      .attr('font-weight', 'bold');
 
   var colors = d3.scaleLinear().domain([0, data.length - 1]).range(["rgb(219, 219, 255)", "blue"])
 
@@ -52,17 +55,20 @@ d3.csv("./data.csv").then( function(data) {
     .range([ height, 0]);
 
   svg.append("g")
-    .call(d3.axisLeft(y));
+    .call(d3.axisLeft(y))
     // calling axisRight appends the labels to the right hand side
+    .attr("class", "y axis")
+    .selectAll("text")
+      .attr('font-weight', 'bold');
 
   // Bars
   svg.selectAll("bar")
     .data(data)
     .join("rect")
       .attr("x", d => x(d.Country))
-      .attr("y", d => y(d.Value))
+      .attr("y", d => y(0))
       .attr("width", x.bandwidth())
-      .attr("height", d => height - y(d.Value))
+      .attr("height", d => height - y(0))
       .attr("fill", (d, i) => colors(i))
       .attr("stroke", "black")
       .attr("opacity", "0.8")
@@ -77,4 +83,20 @@ d3.csv("./data.csv").then( function(data) {
                .duration('50')
                .attr('opacity', '.8');
       })
+
+  // Animation
+  svg.selectAll("rect")
+    .transition()
+    .duration(800)
+    .attr("y", function(d) { return y(d.Value); })
+    .attr("height", function(d) { return height - y(d.Value); })
+    .delay(function(d,i){ return(i*100) })
 })
+
+
+
+// This chooses a color based on the value
+// var colors = d3.scaleQuantize()
+//   .domain([0, 900])
+//   .range(["#5E4FA2", "#3288BD", "#66C2A5", "#ABDDA4", "#E6F598",
+//   "#FFFFBF", "#FEE08B", "#FDAE61", "#F46D43", "#D53E4F", "#9E0142"]);
